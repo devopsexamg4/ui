f99b6c6c84c0d4e8e13bc892b854e07403e4af02
Andreas Askholm <andreas_askholm@hotmail.com>
2024-04-02 11:21:00 +0200

    added a front page, and a template for new pages. added collectstatic to the entry point.

--- UI/frontend/views.py
+++ UI/frontend/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    html = "<html><body>Hello wollolo</body></html>"
-    return HttpResponse(html)
-# return render(request,'home.html',context)
-# render page(template(home.html)) with some request(get/post/..), populate with data in context
+    context = {
+        'title':'Home',
+    }
+    return render(request,'home.html',context)
