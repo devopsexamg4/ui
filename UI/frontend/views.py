476268a0e38604c639a0696c6ace64d9fc3a933a
Andreas Askholm <andreas_askholm@hotmail.com>
2024-04-03 12:38:20 +0200

    added some boilerplate/template to the differetn views

--- UI/frontend/views.py
+++ UI/frontend/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Collect the data to show on the frontpage
 def home(request):
     context = {
         'title':'Home',
     }
     return render(request,'home.html',context)
+
+# collect data to show on the admin page
+def admin(request):
+    pass
+    context = {
+        'title':'Admin',
+    }
+    return render(request, 'admin.html', context)
+
+# collect data to show on the about page
+def about(request):
+    pass
+    context = {
+        'title':'About',
+    }
+    return render(request, 'about.html', context)
+
+# collect data to show on the student page
+def student(request):
+    pass
+    context = {
+        'title':'Student',
+    }
+    return render(request, 'student.html', context)
+
+# collect data to show on the teacher page
+def teacher(request):
+    pass
+    context = {
+        'title':'Teacher',
+    }
+    return render(request, 'teacher.html', context)
