3b10540eb83feeea14f6b93a58b68ea2863b2b9e
Kristoffer Klokker <kristoffer.klokker@gmail.com>
2024-04-10 09:05:53 -0700

    Fixed missing allowed host for production environment

--- UI/UI/settings.py
+++ UI/UI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$ib(#g+&@#cgvyq_=w2vjk!qn0zk%m8$-$pk!)hwg675x_q-u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["34.88.198.48"]
 
 
 # Application definition
