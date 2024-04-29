c5e1723b30a4294ab1d135f0f00021468a6611d3
Andreas Askholm <andreas_askholm@hotmail.com>
2024-04-29 14:23:19 +0200

    added docstrings to forms

--- UI/frontend/forms.py
+++ UI/frontend/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-# form to create a new user
 class SignupForm(UserCreationForm):
+    """form to create a new user"""
     class Meta:
+        """define model and attributes to include in the form"""
         model = User
         fields = ['username','password1','password2']
 
 
-# form to login
 class LoginForm(forms.Form):
+    """form to login"""
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
